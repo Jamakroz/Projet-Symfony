c9628983005992bf0b4432f6368ab090e2e92e16
Simon <simon.barc@hotmail.fr>
2023-10-11 14:00:40 +0200

    Fixture: fixe

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,7 +10,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        /*
         // $product = new Product();
         // $manager->persist($product);
         $site = new Site();
@@ -18,6 +17,5 @@ class AppFixtures extends Fixture
         $manager->persist($site);
 
         $manager->flush();
-    */
     }
 }
